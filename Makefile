d1943d12cf4ddc848e02fcd20bb87f67637ada13
Dennis Kreußel <dnk0@protonmail.com>
2019-11-07 12:23:27 +0100

    improvements to package setup

---
+++ Makefile
@@ -0,0 +1,4 @@
+dist: version
+	python3 setup.py sdist bdist_wheel
+clean: 
+	rm -r build dist *.egg-info
--- setup.py
+++ setup.py
@@ -3,6 +3,9 @@ import setuptools
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
 setuptools.setup(
     name="ids-lib",
     version="0.1.0-dev0",
@@ -12,6 +15,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="",
+    install_requires=requirements,
     packages=setuptools.find_packages(),
     classifiers=[
         "License :: OSI Approved :: MIT License",
